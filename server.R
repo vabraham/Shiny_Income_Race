9ce868f1f18b94efa64cbbeff69c4ffe94771b55
Vineet Abraham <vabraham24@gmail.com>
2015-01-19 23:34:28 -0800

    Updated code comments and added some descriptive text about the data source

--- server.R
+++ server.R
@@ -3,6 +3,7 @@ library(shiny)
 library(ggplot2)
 library(scales)
 
+# Load all support data and convert column names in each dataframe
 lowest_fifth <- read.csv("/srv/shiny-server/data/lowest_fifth.csv", header=TRUE)
 names(lowest_fifth) <- c("Year", "White", "Black", "Hispanic", "Asian", "All Races")
 second_fifth <- read.csv("/srv/shiny-server/data/second_fifth.csv", header=TRUE)
@@ -19,70 +20,67 @@ average <- read.csv("/srv/shiny-server/data/average.csv", header=TRUE)
 names(average) <- c("Year", "White", "Black", "Hispanic", "Asian", "All Races")
 
 shinyServer(
-    # Function takes input from ui.R and returns output objects.
-    function(input, output) {
+  # Function takes input from ui.R and returns output objects.
+  function(input, output) {
     
-        # Get income group from input list.
-        income_group <- reactive({
-          input$income_group  
-        })
-        
-        # Get appropriate dataset based on income group
-        income_cat <- reactive({
-        	if (input$income_group == "Bottom 20% (1-20%)") {
-        		income_cat <- lowest_fifth }
-        	else if (input$income_group == "Second 20% (21-40%)") {
-        		income_cat <- second_fifth }
-        	else if (input$income_group == "Third 20% (41-60%)") {
-        		income_cat <- third_fifth }
-        	else if (input$income_group == "Fourth 20% (61-80%)") {
-        		income_cat <- fourth_fifth }
-        	else if (input$income_group == "Top 20% (81-100%)") {
-        		income_cat <- highest_fifth }
-        	else if (input$income_group == "Top 5% (96-100%)") {
-        		income_cat <- top_5_percent }
-        	else if (input$income_group == "Average") {
-        	  	income_cat <- average }
-        })
-        
-        # Get year from input list.
-        year <- reactive({
-          input$year
-        })
-        
-        # Get race(s) from input list.
-        race <- reactive({
-          input$race  
-        })
-        
-  		# Get income vector
-  		income <- reactive({
-        r <- race()
-        ic <- income_cat()
-        y <- year()
-  			 income <- c()
-  			 for (i in 1:length(r)) {
-  				  newline <- ic[ic$Year==y, r[i]]
-  				  income <- c(income,newline)
-  			  }
-        return(income)
-        })
-        
-        df <- reactive({
-          df <- data.frame(Race = factor(race()), Income = income())
-          return(df)
-        })
-        
-#        output$text1 <- renderTable({
-#          df()
-#        })
-               
-        # Create bar plot
-        output$plot <- renderPlot({
-        	newplot <- ggplot(data=df(), aes(x=Race, y=Income, fill=Race, label=comma(Income))) + geom_bar(stat="identity", width=.75)
-        	newplot <- newplot + scale_y_continuous(labels = comma) + geom_text(fontface="bold") + labs(y = "Income ($)")
-        	newplot <- newplot + theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold"))
-        	print(newplot)
-        })
-    }
+    # Get income group from input list.
+    income_group <- reactive({
+      input$income_group  
+    })
+    
+    # Get appropriate dataset/income category based on income group
+    income_cat <- reactive({
+      if (input$income_group == "Bottom 20% (1-20%)") {
+        income_cat <- lowest_fifth }
+      else if (input$income_group == "Second 20% (21-40%)") {
+        income_cat <- second_fifth }
+      else if (input$income_group == "Third 20% (41-60%)") {
+        income_cat <- third_fifth }
+      else if (input$income_group == "Fourth 20% (61-80%)") {
+        income_cat <- fourth_fifth }
+      else if (input$income_group == "Top 20% (81-100%)") {
+        income_cat <- highest_fifth }
+      else if (input$income_group == "Top 5% (96-100%)") {
+        income_cat <- top_5_percent }
+      else if (input$income_group == "Average") {
+        income_cat <- average }
+    })
+    
+    # Get year from input list.
+    year <- reactive({
+      input$year
+    })
+    
+    # Get race(s) from input list.
+    race <- reactive({
+      input$race  
+    })
+    
+    # Get income vector using race(s), year, and income category
+    income <- reactive({
+      r <- race()
+      ic <- income_cat()
+      y <- year()
+      income <- c()
+      for (i in 1:length(r)) {
+        newline <- ic[ic$Year==y, r[i]]
+        income <- c(income,newline)
+      }
+      return(income)
+    })
+    
+    # Create a dataframe of reactive race and income data from race(s) and income
+    df <- reactive({
+      df <- data.frame(Race = factor(race()), Income = income())
+      return(df)
+    })
+       
+    # Create a reactive barplot from all the inputs
+    output$plot <- renderPlot({
+      newplot <- ggplot(data=df(), aes(x=Race, y=Income, fill=Race, label=comma(Income))) + geom_bar(stat="identity", width=.75)
+      newplot <- newplot + scale_y_continuous(labels = comma) + geom_text(fontface="bold") + labs(y = "Income ($)")
+      newplot <- newplot + theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold"))
+      print(newplot)
+    })
+  }
 )
--- ui.R
+++ ui.R
@@ -2,40 +2,41 @@
 library(shiny)
 
 shinyUI(
-    pageWithSidebar(
-        headerPanel("US Average Household Income by Race and Year"),
-        
-        # Inputs: race, income_group, year
-        sidebarPanel(
-            # Race check box group
-            checkboxGroupInput(
-                inputId = "race",
-                label   = h4("Race, Select 1 or More"),
-                choices = list("White", "Black", "Hispanic", "Asian", "All Races"),
-                selected= c("White", "Black", "Hispanic", "Asian", "All Races")),
-                
-            # Income group radio buttons
-            radioButtons(
-                inputId = "income_group",
-                label   = h4("Income Group"),
-                choices = list("Bottom 20% (1-20%)", "Second 20% (21-40%)", "Third 20% (41-60%)", "Fourth 20% (61-80%)", "Top 20% (81-100%)", "Top 5% (96-100%)", "Average")),
-                
-            # Year (select box)
-			sliderInput(
-                inputId = "year",
-                label 	= h4("Year"),
-                min     = 1972,
-                max     = 2013,
-                value   = 2010,
-                step    = 1,
-                format  = "####")
-        ),
-        
-        # The output goes in mainPanel()
-        mainPanel(
-        	  plotOutput("plot"),
-        	  HTML("<p><em>Data source: United States Census Bureau Mean Household Income Received by Each Fifth and Top 5 Percent</em>"),
-            br(),
-            em(a("Census Bureau Website", href="https://www.census.gov/hhes/www/income/data/historical/household/")))      
-    )
+  pageWithSidebar(
+    headerPanel("US Average Household Income by Race and Year"),
+    
+    # Inputs: race, income_group, year
+    sidebarPanel(
+      # Race check box group
+      checkboxGroupInput(
+        inputId = "race",
+        label   = h4("Race, Select 1 or More"),
+        choices = list("White", "Black", "Hispanic", "Asian", "All Races"),
+        selected= c("White", "Black", "Hispanic", "Asian", "All Races")),
+      
+      # Income group radio buttons
+      radioButtons(
+        inputId = "income_group",
+        label   = h4("Income Group"),
+        choices = list("Bottom 20% (1-20%)", "Second 20% (21-40%)", "Third 20% (41-60%)", "Fourth 20% (61-80%)", "Top 20% (81-100%)", "Top 5% (96-100%)", "Average")),
+      
+      # Year slider input
+      sliderInput(
+        inputId = "year",
+        label 	= h4("Year"),
+        min     = 1972,
+        max     = 2013,
+        value   = 2010,
+        step    = 1,
+        format  = "####")
+    ),
+    
+    # The output goes in mainPanel(). Plot the bargraph based on the inputs.
+    mainPanel(
+      plotOutput("plot"),
+      # Include sourcing information for the data
+      HTML("<p><em>Data source: United States Census Bureau Mean Household Income Received by Each Fifth and Top 5 Percent, Unadjusted for Inflation</em>"),
+      br(),
+      em(a("Census Bureau Website", href="https://www.census.gov/hhes/www/income/data/historical/household/")))      
+  )
 )
